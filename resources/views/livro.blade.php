ce5a3fd40243f73bbd6a654981c794556b621d97
Gabriel Truzzi <gabriel.truzzi@gmail.com>
2020-03-10 12:02:07 -0300

    dia 1 do treinamento

---
+++ resources/views/livro.blade.php
@@ -0,0 +1,21 @@
+<html>
+
+<style>
+body {
+  background-color: gray;
+}
+
+h3 {
+  color: maroon;
+  margin-left: 40px;
+  text-align: center;  
+}
+
+</style>
+<head>
+<title>teste</title>
+</head>
+<body>
+<h3>livro: {{$livro}}
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::get('/{livro}','LivroController@listar');
